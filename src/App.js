138fa8b9c215d87498214efda2f7bb01b638136f
Y-Jimin <jak2899@naver.com>
2024-11-25 22:04:25 +0900

    react로 todo-list 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {setTodo(event.target.value)}
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo == "") {
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [todo, ...currentArray]);
+  }
+  console.log(todos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Your To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          value={todo}
+          placeholder="write your todo" />
+        <button>Add to todos</button>
+      </form>
+      <hr/>
+      <ul>
+        {todos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
